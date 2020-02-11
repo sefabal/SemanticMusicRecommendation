08924011f19fb9316969c450b45f59db8196eed3
sefabal <sefabal@outlook.com>
2020-02-11 12:17:28 +0300

    .gitignore added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -30,11 +30,12 @@ public class Main {
 
             //model.write(System.out);
 
-            //http://musicbrainz.org/artist/24f1766e-9635-4d58-a4d4-9413f9f98a4c#_
+            //http://musicbrainz.org/artist/24f1766e-9635-4d58-a4d4-9413f9f98a4c#_ --> Bach
 
             String queryString = "PREFIX foaf:      <http://xmlns.com/foaf/0.1/>\n" +
                     "SELECT ?artist ?track \n" +
-                    "WHERE \n{\n" +
+                    "WHERE \n" +
+                    "{\n" +
                     "?artist foaf:name \"Johann Sebastian Bach\" .\n" +
                     "?artist foaf:made ?track .\n" +
                     "}";
@@ -42,14 +43,13 @@ public class Main {
             System.out.println("Querying : " + queryString);
 
             Query query = QueryFactory.create(queryString);
-            String artistId = "";
+            String artistId;
             try (QueryExecution qexec = QueryExecutionFactory.create(query, model)) {
                 ResultSet results = qexec.execSelect();
                 System.out.println("Result size = " + results.getResourceModel().size());
-                for (; results.hasNext(); ) {
+                while (results.hasNext()) {
                     QuerySolution soln = results.nextSolution();
                     //RDFNode x = soln.get("varName") ;       // Get a result variable by name.
-
                     Resource r = soln.getResource("artist"); // Get a result variable - must be a resource
                     Resource t = soln.getResource("track");
                     artistId = r.toString();
